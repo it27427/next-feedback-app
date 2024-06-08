c59544045c28dc6b0eebec26ca4a62c5d1992170
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-08 17:31:21 +0600

    config updated

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,12 @@
 const url = process.env.MONGODB_URI;
+const key = process.env.RESEND_API_KEY;
 
 const dev = {
   db: {
     url: url || 'mongodb://localhost:27017/mistry-messages',
+  },
+  api: {
+    resendkey: key,
   },
 };
 
--- src/lib/db.connection.ts
+++ src/lib/db.connection.ts
@@ -1,5 +1,6 @@
-import config from '@/config/config';
 import mongoose from 'mongoose';
+
+import config from '@/config/config';
 
 type ConnectionObject = {
   isConnected?: number;
