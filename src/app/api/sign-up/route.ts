e93cc1ed46b6ab4ddf978e413eec04b6f99582e8
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-09 11:11:31 +0600

    existingVerifiedByEmail

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,3 +1,48 @@
 import connectionDB from '@/lib/db.connection';
 import UserModel from '@/model/User.model';
-import bcrypt from 'bcryptjs';
+// import bcrypt from 'bcryptjs';
+
+// import { sendVerificationEmail } from '@/helpers/sendVerificationEmail';
+
+export const POST = async (req: Request) => {
+  await connectionDB();
+
+  try {
+    const { username, email, password } = await req.json();
+    const existingVerifiedByUsername = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (existingVerifiedByUsername) {
+      Response.json(
+        {
+          success: false,
+          message: 'Username already exists.',
+        },
+        { status: 400 }
+      );
+    }
+
+    const existingVerifiedByEmail = await UserModel.findOne({ email });
+
+    if (existingVerifiedByEmail) {
+      Response.json(
+        {
+          success: false,
+          message: 'Email already exists.',
+        },
+        { status: 400 }
+      );
+    }
+  } catch (error) {
+    console.error('Error registering user', error);
+    return Response.json(
+      {
+        success: false,
+        message: 'Error registering user',
+      },
+      { status: 500 }
+    );
+  }
+};
