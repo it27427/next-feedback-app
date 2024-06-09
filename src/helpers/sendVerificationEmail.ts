0e6820f7b3b820085e1f8d4524d4431a9e2ab738
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-09 10:44:08 +0600

    sendVerificationEmail Done

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,2 +1,29 @@
 import { resend } from '@/lib/resend';
+import { ApiResponse } from '@/types/ApiResponse';
 import VerificationEmail from '../../emails/VerificationEmail';
+
+export const sendVerificationEmail = async (
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> => {
+  try {
+    await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: email,
+      subject: 'Mystry message | Verification Code',
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
+
+    return {
+      success: true,
+      message: 'Verification email send successfully!',
+    };
+  } catch (error) {
+    console.error('Error sending verification email.', error);
+    return {
+      success: false,
+      message: 'Failed to send verification email.',
+    };
+  }
+};
