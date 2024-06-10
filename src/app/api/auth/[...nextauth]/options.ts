a5dced29a142ef16b235fd6cca20e358b9f29c28
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-10 15:48:41 +0600

    options

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -2,3 +2,4 @@ import { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import bcrypt from 'bcryptjs';
 import connectionDB from '@/lib/db.connection';
+import UserModel from '@/model/User.model';
