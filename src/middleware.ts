fcead6b50e6591bacc1fc408c41f024707d743e2
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-11 00:07:32 +0600

    import getToken

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,17 @@
+import { getToken } from 'next-auth/jwt';
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export { default } from 'next-auth/middleware';
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request });
+  const url = request.nextUrl;
+
+  if (token) {
+  }
+
   return NextResponse.redirect(new URL('/home', request.url));
 }
 
