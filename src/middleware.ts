c4d4b13ff849a37d0ea50d731fc5d00b37d9a70e
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-11 00:10:49 +0600

    check token

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,14 @@ export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
 
-  if (token) {
+  if (
+    token &&
+    (url.pathname.startsWith('/sign-in') ||
+      url.pathname.startsWith('/sign-up') ||
+      url.pathname.startsWith('/verify') ||
+      url.pathname.startsWith('/'))
+  ) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
   }
 
   return NextResponse.redirect(new URL('/home', request.url));
