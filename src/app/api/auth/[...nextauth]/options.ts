6ab71483c4635090ffb1ecf4ab7d5ee3b671e83f
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-10 16:06:44 +0600

    credential options next-auth

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -3,3 +3,38 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 import bcrypt from 'bcryptjs';
 import connectionDB from '@/lib/db.connection';
 import UserModel from '@/model/User.model';
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: 'credentials',
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials: any): Promise<any> {
+        await connectionDB();
+
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.identifier.email },
+              { username: credentials.identifier.username },
+            ],
+          });
+
+          if (!user) {
+            throw new Error('No user found with this email.');
+          }
+
+          if (!user.isVerified) {
+            throw new Error('Please verify your account first.');
+          }
+        } catch (error: any) {
+          throw new Error(error);
+        }
+      },
+    }),
+  ],
+};
