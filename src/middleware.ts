194c5a6e79e29bbbdd1db00dcd564dd06c68078b
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-11 12:47:51 +0600

    middlewear updated and moved in src directory

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,7 +19,11 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL('/dashboard', request.url));
   }
 
-  return NextResponse.redirect(new URL('/', request.url));
+  if (!token && url.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/sign-in', request.url));
+  }
+
+  return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
